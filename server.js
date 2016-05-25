319d548aa70ab7a7c020887a12b1dde243a895bf
joseph dias <josephdias92@gmail.com>
2016-05-25 22:22:58 +0530

    updated mongoose to v4

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://admin:joeyk@ds011963.mlab.com:11963/heroku_d67d1q61'); // connect to our database
+mongoose.connect('mongodb://joseph:joeyk@ds011963.mlab.com:11963/heroku_d67d1q61'); // connect to our database
 
 var Board     = require('./app/models/board');
 // configure app to use bodyParser()
@@ -23,9 +23,9 @@ var router = express.Router();              // get an instance of the express Ro
 router.route('/boards')
 
     .post(function(req, res) {
-        
-        var board = new Board();      
-        board.name = req.body.name;  
+
+        var board = new Board();
+        board.name = req.body.name;
 
         board.save(function(err) {
             if (err)
@@ -33,7 +33,7 @@ router.route('/boards')
 
             res.json({ message: 'Board created!' });
         });
-        
+
     }).get(function(req, res) {
         Board.find(function(err, b) {
             if (err)
